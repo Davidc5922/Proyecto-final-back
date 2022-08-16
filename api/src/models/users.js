3aabd8a70bfc2d3daf5ab2b437baef96d2f9b87a
deathstroke459 <101474305+deathstroke459@users.noreply.github.com>
2022-08-16 14:41:48 -0500

    first commit

--- api/src/models/users.js
+++ api/src/models/users.js
@@ -3,10 +3,50 @@ const { DataTypes } = require('sequelize');
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('users', {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
+  sequelize.define(
+    'users',
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      username: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false,
+      },
+      profilePic: {
+        type: DataTypes.STRING,
+      },
+      password: {
+        type: DataTypes.STRING,
+      },
+      banned: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
+      },
+      admin: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
+        allowNull: false,
+      },
+      authzero: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
+      },
     },
-  });
+    {
+      timestamps: true,
+    }
+  );
 };
