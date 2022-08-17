5d00f4187bf9eac2945569ef2f0018d0fd86295a
chaque1111 <99604927+chaque1111@users.noreply.github.com>
2022-08-17 20:53:54 -0300

    prueba

---
+++ src/routes/index.js
@@ -0,0 +1,29 @@
+const e = require('express');
+const { Router } = require('express');
+const users = require('./users')
+
+// Importar todos los routers;
+// Ejemplo: const authRouter = require('./auth.js');
+const {User} = require("../db")
+
+const router = Router();
+// Configurar los routers
+// Ejemplo: router.use('/auth', authRouter);
+
+router.use('/users',users)
+
+
+
+router.get("/", async (req,res)=>{
+
+    res.send("funciona :D")
+   
+})
+
+router.get("/products", async (req,res)=>{
+
+     res.send("ruta de prueba")
+    
+})
+
+module.exports = router;
