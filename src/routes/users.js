1d6c7461a46d2d9de9244c464e622212b0c67f22
deathstroke459 <101474305+deathstroke459@users.noreply.github.com>
2022-08-17 17:31:22 -0500

    ruta - post

---
+++ src/routes/users.js
@@ -0,0 +1,31 @@
+const { Router } = require('express');
+const router = Router()
+const {User} = require("../db")
+
+router.post('*', async (req, res,next) => {
+    let {
+        name,
+        surname,
+        age,
+        email,
+        password,
+        location
+    } = req.body
+    
+    try {
+       // let height=`${heightMin}-${heightMax}`
+    // let weight=`${weightMin}-${weightMax}`
+    let UserCreated = await User.create({
+        name,
+        surname,
+        age,
+        email,
+        password,
+        location
+    })
+    
+    res.status(200).send("usuario creado") 
+    } catch (e) {
+        next(e);
+    }
+})
