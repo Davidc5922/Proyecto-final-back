b9bae9d985ddfd1b9ef5bdb8b09f3654994882cd
chaque1111 <99604927+chaque1111@users.noreply.github.com>
2022-08-19 00:03:15 -0300

    agregando filtro de categorias

--- src/Controllers/filters.js
+++ src/Controllers/filters.js
@@ -1,27 +1,25 @@
 const {Product,Category} = require("../db")
 
 const filterByCategory = async (category) => {
-//  try {
-//      const categoryId = await Category.findOne({
-//          where: {name : category},
-//          include:[{
-//             attributes: ["name"],
-//             through: {
-//               attributes: [],
-//             },
-//        }]
-//      })
-//     console.log(categoryId)
-
-//  } catch (e) {
+ try {
+     const Id = await Category.findOne({
+         where: {name : category},
+     })
+     console.log(Id.id)
+     const allInfo = await Product.findAll();
+     const filterByCategory = allInfo.filter(el => el.categoryId === Id.id)
+ 
     
-//  }
+    return filterByCategory;
+ } catch (e) {
+    
+ }
 }
 
 const filterByGenre = async (genre) => {
    try {
         const allInfo = await Product.findAll();
-        const FilerByGenre = allInfo.filter(e => e.genre===genre)
+        const FilerByGenre = allInfo.filter(e => e.genre === genre)
         return FilerByGenre
    } catch (e) {
        console.log(e)
