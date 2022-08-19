8d1abe41ebed7e8ced843b60c920f1389d4eb40b
deathstroke459 <101474305+deathstroke459@users.noreply.github.com>
2022-08-18 20:02:40 -0500

    ruta products get ID

--- src/routes/Products.js
+++ src/routes/Products.js
@@ -15,4 +15,76 @@ router.get('/', async (req, res) => {
 	res.send('funciona :D');
 });
 
+router.get('/:Id',async(req, res,next) => {
+  const { Id } = req.params;
+  console.log(Id)
+  if (Id) {
+      let product = await Product.findByPk(Id);
+  let product_ID = product.map(e=>{
+    return {
+      name:e.name,
+      brand:e.brand,
+      price:e.price,
+      stock:e.stock,
+      image:e.image,
+      sold:e.sold,
+      size:e.size,
+      score:e.score,
+      genre:e.genre
+    }
+
+
+  })
+          res.status(200).json(product_ID) 
+         
+  } 
+})
+router.delete("/:id", async function (req, res) {
+  const { id } = req.params;
+  try {
+    if (id) {
+      await Product.destroy({
+        where: { id: id },
+      });
+      res.send({ msg: "producto eliminado" });
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+
+
+router.post('*', async (req, res,next) => {
+  let {
+      name,
+      brand,
+      price,
+      stock,
+      image,
+      sold,
+  size,
+  score,
+  genre
+  } = req.body
+  
+  try {
+  let newProduct= await Product.create({
+      name,
+      brand,
+      price,
+      stock,
+      image,
+      sold,
+  size,
+  score,
+  genre
+  })
+  
+  res.status(200).send("Producto Creado") 
+  } catch (e) {
+      next(e);
+  }
+})
+
 module.exports = router;
