258fbcc40ee55a3b2a7f63f11f4ef0a21bfdd3a8
deathstroke459 <101474305+deathstroke459@users.noreply.github.com>
2022-08-19 16:17:15 -0500

    install prettier David

--- src/db.js
+++ src/db.js
@@ -61,24 +61,13 @@ sequelize.models = Object.fromEntries(capsEntries);
 const { User, Product, Buy, Category } = sequelize.models;
 
 // Aca vendrian las relaciones
-// Product.hasMany(Reviews);
-// Product.belongsToMany(Users, {through: "users_product"})
-// Users.belongsToMany(Product, {through: "users_product"})
+
 User.hasMany(Buy);
 Buy.belongsTo(User);
 
 Product.belongsToMany(Buy, { through: 'Products_Buys' });
 Buy.belongsToMany(Product, { through: 'Products_Buys' });
-
-
-Category.hasMany(Product);
-Product.belongsTo(Category);
-
-// User.hasMany(Buy);
-// Buy.belongsTo(User);
-
-// Product.belongsToMany(Buy, { through: 'Products_Buys' });
-// Buy.belongsToMany(Product, { through: 'Products_Buys' });
+ //xd       
 
 Category.hasMany(Product);
 Product.belongsTo(Category);
