280b55bf47eafa269f5b3339e4cfed8eb6d88854
chaque1111 <99604927+chaque1111@users.noreply.github.com>
2022-08-24 19:09:22 -0300

    agregando ruta create user/ alex

--- src/Controllers/user.js
+++ src/Controllers/user.js
@@ -1,8 +1,16 @@
 const {User} = require("../db.js");
 
 
+const findByName = async (name) => {
+    try {
+          const user = await User.findAll({where: {username: name}})
+              return user;
+    } catch (error) {
+        console.log(error)
+    }
+}
 
 
 module.exports = {
-    findUserByPk,
+  findByName,
 }
