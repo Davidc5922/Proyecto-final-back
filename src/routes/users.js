c773affe1fe636037af938fe906d7405de441df2
chaque1111 <99604927+chaque1111@users.noreply.github.com>
2022-08-27 15:11:22 -0300

    uwuw

--- src/routes/users.js
+++ src/routes/users.js
@@ -6,22 +6,6 @@ const { User, Product} = require('../db');
 
 
 //USUARIOS
-router.get('/user/:email', async (req, res) => {
-	try {
-		const { email } = req.params;
-		const user = await User.findOne({ where: { email: email } });
-		console.log(user);
-		let bool = false;
-
-		if (user) {
-			return res.send(true);
-		} else {
-			return res.send(false);
-		}
-	} catch (e) {
-		console.log(e);
-	}
-});
 
 router.get('/', async (req, res, next) => {
 	try {
